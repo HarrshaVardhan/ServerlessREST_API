051efb9d560dc3531d4c522c418757844f81dabd
Meisam Sharahi <meisam.bit@outlook.com>
2022-03-28 01:03:48 +0300

    Updated

--- src/query.py
+++ src/query.py
@@ -2,8 +2,10 @@ import os
 import boto3
 from datetime import date, timedelta
 
+
 def date_just(timestamp):
     return timestamp.strftime("%d/%m/%Y")
+
 
 def query(event, context):
     bucket_name = os.environ['BUCKET_NAME']
@@ -13,11 +15,12 @@ def query(event, context):
     timestamp_week_ago = date_just(timestamp_week_ago)
     timestamp_current = date_just(timestamp_current)
     stmnt = f"SELECT * FROM notes WHERE CreatedAt BETWEEN '{timestamp_week_ago}' AND '{timestamp_current}'"
-    qres = client.execute_statement(Statement= stmnt)
-    x=len(qres["Items"])
-    output=f"The number of added item/s in the last week : {x}"
+    qres = client.execute_statement(Statement=stmnt)
+    x = len(qres["Items"])
+    output = f"The number of added item/s in the last week : {x}"
     s3 = boto3.client('s3')
-    fileName=str(timestamp_current) + '.txt'
+    timestamp_current = ''.join(item for item in timestamp_current if item.isalnum())
+    fileName = timestamp_current + '.txt'
     s3.put_object(Bucket=bucket_name, Key=fileName, Body=output)
     response = {
         'statusCode': 200,
