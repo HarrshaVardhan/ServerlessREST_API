740c6c3b83cfe26202b607b1e4b957f6d2093d4b
Meisam Sharahi <meisam.bit@outlook.com>
2022-03-27 23:26:51 +0300

    Updated

---
+++ src/query.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+from datetime import date, timedelta
+
+def date_just(timestamp):
+    return timestamp.strftime("%d/%m/%Y")
+
+def query(event, context):
+    
+    client = boto3.client('dynamodb')
+    timestamp_week_ago = date.today() - timedelta(days=7)
+    timestamp_current = date.today()
+    timestamp_week_ago = date_just(timestamp_week_ago)
+    timestamp_current = date_just(timestamp_current)
+    stmnt = f"SELECT * FROM notes WHERE CreatedAt BETWEEN '{timestamp_week_ago}' AND '{timestamp_current}'"
+    response = client.execute_statement(Statement= stmnt)
+    x=len(response["Items"])
+    
+    return f"The number of added in a week ago : {x} Item/s"
